7c1c0b175778cb64c8d80292da96d377e67f1044
avmedvedskiy <a.v.medvedskiy@gmail.com>
2024-05-28 15:07:52 +0300

    add anchors extensions

---
+++ Runtime/Infrastructure/Anchors/AnchorsControllerExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Cysharp.Threading.Tasks;
+
+namespace UISystem
+{
+    public static class AnchorsControllerExtensions
+    {
+        public static async UniTask<(IAnchor from, IAnchor to)> ShowAnchor(this IAnchorsController anchorsController, int id1, int id2) =>
+            await UniTask.WhenAll(
+                anchorsController.ShowAnchor(id1), 
+                anchorsController.ShowAnchor(id2));
+
+        public static async UniTask HideAnchor(this IAnchorsController anchorsController, int id1, int id2) =>
+            await UniTask.WhenAll(
+                anchorsController.HideAnchor(id1),
+                anchorsController.HideAnchor(id2)
+            );
+    }
+}
