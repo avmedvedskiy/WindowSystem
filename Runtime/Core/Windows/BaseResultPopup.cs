f35119d7c22ebe2a512c7eb102ee2916134567ae
avmedvedskiy <a.v.medvedskiy@gmail.com>
2024-09-13 11:24:41 +0300

    add base result popup

---
+++ Runtime/Core/Windows/BaseResultPopup.cs
@@ -0,0 +1,15 @@
+﻿using Cysharp.Threading.Tasks;
+
+namespace UISystem
+{
+    public abstract class BaseResultPopup<TPayload,TResult> : BaseWindow<TPayload>, IResultWindow<TResult>
+    {
+        public UniTaskCompletionSource<TResult> Result { get; private set; }
+
+        protected override UniTask OnOpenAsync(TPayload payload)
+        {
+            Result = new();
+            return base.OnOpenAsync(payload);
+        }
+    }
+}
