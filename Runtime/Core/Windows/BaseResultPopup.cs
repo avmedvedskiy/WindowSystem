3aea877b8628d4ff295a13b07fc4889fa186b9b6
avmedvedskiy <a.v.medvedskiy@gmail.com>
2025-09-19 23:19:27 +0300

    try fix popups error

--- Runtime/Core/Windows/BaseResultPopup.cs
+++ Runtime/Core/Windows/BaseResultPopup.cs
@@ -4,12 +4,6 @@ namespace UISystem
 {
     public abstract class BaseResultPopup<TPayload,TResult> : BaseWindow<TPayload>, IResultWindow<TResult>
     {
-        public UniTaskCompletionSource<TResult> Result { get; private set; }
-
-        protected override UniTask OnOpenAsync(TPayload payload)
-        {
-            Result = new();
-            return base.OnOpenAsync(payload);
-        }
+        public UniTaskCompletionSource<TResult> Result { get; } = new();
     }
 }
