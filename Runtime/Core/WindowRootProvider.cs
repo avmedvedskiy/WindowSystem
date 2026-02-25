8b24032bd1d3d84665a945d84ddcd3358c03e021
avmedvedskiy <a.v.medvedskiy@gmail.com>
2026-02-25 15:52:38 +0200

    add overlayroot

--- Runtime/Core/WindowRootProvider.cs
+++ Runtime/Core/WindowRootProvider.cs
@@ -6,12 +6,15 @@ namespace UISystem
     public class WindowRootProvider : MonoBehaviour, IWindowRootProvider
     {
         [SerializeField] private Transform _root;
+        [SerializeField] private Transform _overlayRoot;
         
         public Transform Root => _root;
+        public Transform OverlayRoot => _overlayRoot;
 
         private void OnValidate()
         {
             _root ??= transform;
+            _overlayRoot ??= transform;
         }
     }
 }
